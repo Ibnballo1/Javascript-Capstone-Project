21991ec73ec62a9f4db01a56356d632bbb2500f3
Ibnballo1 <belloabdullateef035@gmail.com>
2022-07-02 08:53:07 +0100

    Fix errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './style.css';
 import './images/logo.png';
-import displayList from './display_list.js';
+import displayList from './modules/display_list.js';
 import popup from './modules/createPopup.js';
 
 displayList();
